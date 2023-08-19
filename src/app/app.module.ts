9016d2935623fe89e7ab333acbd5dfa7b4165efa
Cait Johnson <caitjohnson@wrightsgate.net>
2023-08-19 01:46:19 -0800

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+/**
+ * Title: app.module.ts
+ * Author: Caitlynne Johnson
+ * Date: 8/5/23
+ */
+
+// imports statements
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BaseLayoutComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/task-management/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Title: tasks.component.ts
+ * Author: Caitlynne Johnson
+ * Date: 8/5/2023
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
